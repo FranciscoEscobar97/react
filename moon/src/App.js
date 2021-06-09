f16eb13fa25884f7feac5ef883ae83b2bf7d8afe
FranciscoEscobar97 <80058234+FranciscoEscobar97@users.noreply.github.com>
2021-06-08 19:53:49 -0500

    Add files via upload

--- moon/src/App.js
+++ moon/src/App.js
@@ -3,8 +3,39 @@ import './App.css';
 
 function App() {
   return (
+
+
+
+
     <div className="App">
       <header className="App-header">
+      <nav class="navbar navbar-expand-lg navbar-light tamaño"> 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio<span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><span class="iconify" data-icon="et:tools" data-inline="false"></span>Taller</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Categorias
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Rotomartillos</a>
+                        <a class="dropdown-item" href="#">Taladros</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACTENOS</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
